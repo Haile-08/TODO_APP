6f17d9d10948aeb18b65121d8281e0e799c1104a
Haile-08 <haile4cmd@gmail.com>
2022-07-23 20:21:47 +0300

    started the add btn commponent

---
+++ client/src/components/add_btn/add_btn.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Add_Btn() {
+  return <></>;
+}
+export default Add_Btn;
--- client/src/pages/AppLayout.js
+++ client/src/pages/AppLayout.js
@@ -1,5 +1,7 @@
 import Searchbar from "../components/search/searchbar";
 import Items from "../components/items/items";
+import Add_Btn from "../components/add_btn/add_btn";
+
 import "../css/App.css";
 
 import React from "react";
@@ -10,6 +12,7 @@ function AppLayout() {
       <h1 className="h">Todo List</h1>
       <Searchbar />
       <Items />
+      <Add_Btn />
     </div>
   );
 }
