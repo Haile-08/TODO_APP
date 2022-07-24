e05bbca9788453d37a1459e05343bf495b7967e8
Haile-08 <haile4cmd@gmail.com>
2022-07-24 13:16:18 +0300

    added modal

---
+++ client/src/components/close/close.js
@@ -0,0 +1,13 @@
+import React from "react";
+import close from "../../assets/close.svg";
+import "../../css/close.css";
+
+function Close() {
+  return (
+    <button className="closebtn">
+      <p>X</p>
+    </button>
+  );
+}
+
+export default Close;
--- client/src/components/modal/post/postmodal.js
+++ client/src/components/modal/post/postmodal.js
@@ -1,21 +1,26 @@
 import React from "react";
 import Add_Btn from "../../add_btn/add_btn";
+import Close from "../../close/close";
 import "../../../css/postmodal.css";
 
 function PostModal() {
   return (
-    <div className="container">
-      <div className="txtarea">
-        <input
-          type="text"
-          placeholder="type something..."
-          className="txtinput"
+    <div className="postmodalcontainer">
+      <div className="closebtncont">
+        <Close />
+      </div>
+      <div className="posttxt">
+        <textarea
+          className="postinput"
+          rows="10"
+          cols="50"
+          placeholder="type something .."
         />
       </div>
-      <div className="imginsert">
+      <div className="postimginsert">
         <input type="file" id="img" name="img" accept="image/*" />
       </div>
-      <div className="btnarea">
+      <div className="postbtnarea">
         <Add_Btn />
       </div>
     </div>
